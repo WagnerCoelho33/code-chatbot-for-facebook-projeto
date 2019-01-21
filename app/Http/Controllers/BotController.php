3cf846a0aa4fe48d36dbd94539b0756548ae6388
WagnerCoelho33 <wagnermsc2014@gmail.com>
2019-01-21 21:54:46 -0200

    BotControlller

---
+++ app/Http/Controllers/BotController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use CodeBot\WebHook;
+use Illuminate\Http\Request;
+use CodeBot\SendRequest;
+use CodeBot\Message\Text;
+use CodeBot\CallSendApi;
+
+class BotController extends Controller
+{
+    public function subscribe()
+    {
+        $webhook = new WebHook;
+        $subscribe = $webhook->check('asd');
+        if (!$subscribe) 
+        {
+            abort(403, 'Unauthorized action.');
+        }
+        return $subscribe;
+    }
+
+    public function receiveMessage(Request $request)
+    {
+        $sender = new SendRequest;
+        $senderId = $sender->getSenderId();
+        $message = $sender->getMessage();
+
+        $text = new Text($senderId);
+        $callSendApi = new CallSendApi('asdasd');
+
+        $callSendApi->make($text->message('Oii, eu sou um bot... '));
+        $callSendApi->make($text->message('Você digitou: '. $message));
+
+        return '';
+    }
+}
